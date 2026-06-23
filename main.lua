765d63ae8af021428b8e08ad42c5dcd44a15fb63
Hassan Mahmoud <officialhassansmail@gmail.com>
2026-06-23 21:15:09 +0300

    - add collision detection

--- main.lua
+++ main.lua
@@ -1,33 +1,60 @@
 -- -1 to use adaptive vsync (where supported)
-SCREEN_OPTIONS = { fullscreen = true, borderless = false, vsync = -1, centered = true }
 
 function love.load()
+	local screen_opts = { fullscreen = true, borderless = false, vsync = 1, centered = false }
 	love.window.setTitle("DVD Bouncing")
 	-- If width or height is 0, setMode will use the width and height of the desktop.
-	love.window.setMode(0, 0, SCREEN_OPTIONS)
+	love.window.setMode(0, 0, screen_opts)
 	LOGO = love.graphics.newImage("dvd-video.png")
-	LOGO_WIDTH = LOGO:getWidth()
-	LOGO_HEIGHT = LOGO:getHeight()
+	SCALE = 0.5
+	LOGO_WIDTH = LOGO:getWidth() * SCALE
+	LOGO_HEIGHT = LOGO:getHeight() * SCALE
 	WIDTH = love.graphics.getWidth()
 	HEIGHT = love.graphics.getHeight()
 	X, Y = WIDTH / 2, HEIGHT / 2
+	local speed = 240
+	DX, DY = speed, speed
 end
 
 function love.update(dt)
-	local speed = 240
-	local dx, dy = speed, speed
-	X = X + dx * dt
-	Y = Y + dy * dt
+	X = X + DX * dt
+	Y = Y + DY * dt
+	collision_detection()
 end
 
 function love.draw()
-	local r, g, b = love.math.colorFromBytes(132, 163, 238)
-	love.graphics.setBackgroundColor(r, g, b)
-	love.graphics.draw(LOGO, X, Y, 0, 0.5, 0.5, LOGO_WIDTH / 2, LOGO_HEIGHT / 2)
+	love.graphics.draw(LOGO, X, Y, 0, SCALE, SCALE, LOGO_WIDTH / 2, LOGO_HEIGHT / 2)
 end
 
 function love.keypressed(key)
 	if key == "escape" or key == "space" then
 		love.event.quit()
+	end
+end
+
+function collision_detection()
+	local half_w = LOGO_WIDTH / 2
+	local half_h = LOGO_HEIGHT / 2
+
+	-- detect vertical collsion
+	if Y - half_h <= 0 then
+		Y = half_h
+		DY = -DY
+	end
+
+	if Y + half_h >= HEIGHT then
+		Y = HEIGHT - half_h
+		DY = -DY
+	end
+
+	-- detect horizontal collsion
+	if X - half_w <= 0 then
+		X = half_w
+		DX = -DX
+	end
+
+	if X + half_w >= WIDTH then
+		X = WIDTH - half_w
+		DX = -DX
 	end
 end
